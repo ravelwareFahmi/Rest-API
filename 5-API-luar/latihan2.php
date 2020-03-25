270f9838debd615d1a2943bae84e72941f67944f
ravelwareFahmi <60088312+ravelwareFahmi@users.noreply.github.com>
2020-03-25 21:58:33 +0700

    add see detail in movie search

--- 5-API-luar/latihan2.php
+++ 5-API-luar/latihan2.php
@@ -56,7 +56,7 @@
                     <div class="input-group mb-3">
                         <input type="text" class="form-control" id="search-input" placeholder="Search Movie">
                         <div class="input-group-append">
-                            <button class="btn btn-dark" id="search-button" type="button">Button</button>
+                            <button class="btn btn-dark" id="search-button" type="button">Search</button>
                         </div>
                     </div>
                 </div>
@@ -88,3 +88,23 @@
         </script>
     </body>
 </html>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Detail Movies</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
--- 5-API-luar/script.js
+++ 5-API-luar/script.js
@@ -26,7 +26,7 @@ function searchMovie() {
                         `</h5>
                                     <p class="card-text">` + data.Year +
                         `</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    <a href="#" class="btn btn-primary seeDetail" data-toggle="modal" data-id="`+data.imdbID+`" data-target="#exampleModal">See Detail</a>
                                 </div>
                             </div>
                             </div>
@@ -34,7 +34,7 @@ function searchMovie() {
                     );
                 });
 
-                ('#search-input').val(''); //kosongkan form search movie setelah data didapatkan
+                $('#search-input').val(''); //kosongkan form search movie setelah data didapatkan
             } else {
                 // jika search tidak ada
                 $('#movie-list').html(
@@ -55,6 +55,55 @@ $('#search-button').on('click', function(){
 })
 
 $('#search-input').on('keyup', function(e){
-    if(e.which === 13)
+    if(e.which === 13) //13 merupakan keyCode dari enter
      searchMovie();
+})
+
+// Cari class .see detail dalam ID MOVIE LIST 
+$('#movie-list').on('click','.seeDetail', function(){
+    // console.log($(this).data('id'));
+    $.ajax({
+        type: "get",
+        url: "http://www.omdbapi.com",
+        dataType: "json",
+        data: {
+            "apikey": "5ad1f444",
+            "i": $(this).data('id')
+        },
+        success: function (response) {
+            if (response.Response == 'True') {
+                console.log('resonse');
+                
+                $('.modal-body').html(`
+                    <div class="container>
+                        <div class="row">
+                            <div class="col-md-4">
+                            <img src="`+ response.Poster +`" class="img-fluid">
+                            </div>
+                            <div class="col-md-8>
+                                <ul class="list-group">
+                                    <li class="list-group-item">
+                                        <h3>`+response.Title+`</h3>
+                                    </li>
+                                    <li class="list-group-item"> Released
+                                        `+response.Released+`
+                                    </li>
+                                    <li class="list-group-item"> Genre
+                                        `+response.Genre+`
+                                    </li>
+                                    <li class="list-group-item"> Language
+                                        `+response.Language+`
+                                    </li>
+                                    <li class="list-group-item"> Country
+                                        `+response.Country+`
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                `)
+            }
+        }
+    });
+    
 })
